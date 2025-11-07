f0dd71287005cd902541e318f93662545efeb007
https://github.com/DeshnaPatodi/first-repository.git <patodideshna04@gmail.com>
2025-11-07 11:43:30 +0530

    Added footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+       
+
+<footer className="px-6 md:px-16 lg:px-24 xl:px-32 w-full text-sm text-slate-500 bg-white pt-10 mt-20">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
+        <div className="sm:col-span-2 lg:col-span-1">
+            <img className='h-9' src={assets.logo2} alt="logo"/>
+            <p className="text-sm/7 mt-6">
+            Experience the power of AI with GradeMate AI.<br/>
+            Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow.
+            </p>
+        </div>
+        <div className="flex flex-col lg:items-center lg:justify-center">
+            <div className="flex flex-col text-sm space-y-2.5">
+                <h2 className="font-semibold mb-5 text-gray-800">Company</h2>
+                <a className="hover:text-slate-600 transition" href="#">About us</a>
+                <a className="hover:text-slate-600 transition" href="#">Careers</a>
+                <a className="hover:text-slate-600 transition" href="#">Contact us</a>
+                <a className="hover:text-slate-600 transition" href="#">Privacy policy</a>
+            </div>
+        </div>
+        <div>
+            <h2 className="font-semibold text-gray-800 mb-5">Subscribe to our newsletter</h2>
+            <div className="text-sm space-y-6 max-w-sm">
+                <p>Copyright 2025 @GradeMate. All the Right Reserved</p>
+                <div className="flex items-center justify-center gap-2 p-2 rounded-md bg-indigo-50">
+                    <input className="focus:ring-2 ring-indigo-600 outline-none w-full max-w-64 py-2 rounded px-2" type="email" placeholder="Enter your email"/>
+                    <button className="bg-primary px-4 py-2 text-white rounded cursor-pointer">Subscribe</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p className="py-4 text-center border-t mt-6 border-slate-200">
+        Copyright 2025 © <a href="https://prebuiltui.com">PrebuiltUI</a> All Right Reserved.
+    </p>
+</footer>
+    </div>
+  )
+}
+
+export default Footer
--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -35,7 +35,7 @@ const Testimonial = () => {
     <div className="px-4 sm:px-20 xl:px-32 py-24">
       <div className="text-center">
         <h2 className="text-slate-700 text-[42px] font-semibold">
-          Loved by Creators
+          Loved by Students
         </h2>
         <p className="text-gray-500 max-w-lg mx-auto">
           Don't just take our word for it. Here's what our users are saying.
